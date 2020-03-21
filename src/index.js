9a104a38365bb6885754588daee14705065459d6
pengyu07 <pengyu07@maoyan.com>
2020-03-21 22:03:30 +0800

    xlsx批量导入

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
 const puppeteer = require('puppeteer');
-var path = require("path");
-var urlencode = require('urlencode');
+const path = require("path");
+const urlencode = require('urlencode');
 const cookies = require('./cookie');
+const xlsx = require('node-xlsx')
 
-
-let chName = '郑州迎云实业有限公司'
 let browser
 (async () => {
   browser = await puppeteer.launch({
-  // executablePath: path.join(__dirname, './chromium/Chromium.app/Contents/MacOS/Chromium'),
-  // 关闭headless模式, 会打开浏览器
-  headless: false,
-  args: ['--no-sandbox', '--disable-dev-shm-usage'],
-});
+    headless: true,
+    args: ['--no-sandbox', '--disable-dev-shm-usage'],
+  });
+  let datas = await parseFile()
+  
+  for(let i = 0; i < 10; i++ ){
+    await qcc(datas[i]);  
+  }
+    
+  process.exit();
 
-  await qcc(chName);
 })();
 
-async function qcc(chName){
+async function parseFile(){
+  let sheets = xlsx.parse(path.resolve(__dirname,'../files/ddd.xlsx'));
+  return datas = sheets[0].data.slice(1)
+}
+
+async function qcc(data){
+  let [no, index, name, addrIndex, sendAddr, contact, phone, method, queryAddr, isSame, addrIndex2, ...orthers] = data
   try{
     const page = await browser.newPage();
     for(let i = 0; i < cookies.length; i++){
@@ -25,7 +34,7 @@ async function qcc(chName){
     }
   
     await page.setViewport({ width: 1920, height: 1080 });
-    await page.goto(`https://www.qcc.com/search?key=${urlencode(chName)}`, {timeout:60000, waitUntil: 'domcontentloaded'});
+    await page.goto(`https://www.qcc.com/search?key=${urlencode(name)}`, {timeout:60000, waitUntil: 'domcontentloaded'});
     const preloadHref = await page.$eval('.ma_h1', el => el.href);
     await page.goto(preloadHref, {timeout:60000, waitUntil: 'domcontentloaded'});
 
@@ -33,19 +42,20 @@ async function qcc(chName){
       let okButtonElement = await page.$('modal-footer .btn.btn-primary');
       await okButtonElement.click()
     } catch (error) {
-      console.log('no button')
     }
   
     let element = await page.$('#company-top'); //企业详情
     await element.screenshot({
-        path: `${path.resolve(__dirname,'../files')}/${chName}.png`
+        path: `${path.resolve(__dirname,'../files')}/${addrIndex}${name}.png`
     });
   
     const addr = await page.$$eval('.row .cvlu a', els => els[els.length - 2].innerHTML);
-    console.log(addr)
-    process.exit();
+    
+    //输出 
+    console.log(no, index, name, addrIndex, sendAddr, contact, phone, method, addr, sendAddr.trim() == addr.trim() ? '是' : '否', addrIndex2, ...orthers)
+    page.close()
   } catch(e){
-    console.log('操作失败', e)
+    console.log(`${no},${index},${name} 操作失败`, e)
   }
 
 }
