2748f08c1b3ab287339b68439771d1f957c0342b
pengyu07 <pengyu07@maoyan.com>
2020-03-21 18:05:29 +0800

    修改cookie格式

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
+var path = require("path");
 var urlencode = require('urlencode');
-const { cookie1, cookie2 , cookie3, cookie4, cookie5, cookie6, cookie7, cookie8, cookie9,cookie10} = require('./cookie');
+const cookies = require('./cookie');
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -10,16 +11,9 @@ const { cookie1, cookie2 , cookie3, cookie4, cookie5, cookie6, cookie7, cookie8,
     args: ['--no-sandbox', '--disable-dev-shm-usage'],
   });
   const page = await browser.newPage();
-  await page.setCookie(cookie1)
-  await page.setCookie(cookie2)
-  await page.setCookie(cookie3)
-  await page.setCookie(cookie4)
-  await page.setCookie(cookie5)
-  await page.setCookie(cookie6)
-  await page.setCookie(cookie7)
-  await page.setCookie(cookie8)
-  await page.setCookie(cookie9)
-  await page.setCookie(cookie10)
+  for(let i = 0; i < cookies.length; i++){
+    await page.setCookie(cookies[i])
+  }
 
   await page.setViewport({ width: 1920, height: 1080 });
 
@@ -35,7 +29,7 @@ const { cookie1, cookie2 , cookie3, cookie4, cookie5, cookie6, cookie7, cookie8,
 
   let element = await page.$('#company-top'); //企业详情
   await element.screenshot({
-      path: '../files/element.png'
+      path: `${path.resolve(__dirname,'../files')}/${chName}.png`
   });
 
   const addr = await page.$$eval('.row .cvlu a', els => els[els.length - 2].innerHTML);
